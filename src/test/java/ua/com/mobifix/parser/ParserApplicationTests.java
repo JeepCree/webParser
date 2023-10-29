41b1e40dabfdc2fdae7c7e812438afe687930056
Dima <76869662+JeepCree@users.noreply.github.com>
2023-10-29 13:50:30 +0200

    Initial commit

---
+++ src/test/java/ua/com/mobifix/parser/ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.mobifix.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
