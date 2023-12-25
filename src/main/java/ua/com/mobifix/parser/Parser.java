0f21500bcfb2227e98046e3c8fef453b26975bf3
dima2 <dima.26.2013@gmail.com>
2023-12-25 17:30:18 +0200

    save 202312251730 (add Parser)

--- src/main/java/ua/com/mobifix/parser/Parser.java
+++ src/main/java/ua/com/mobifix/parser/Parser.java
@@ -11,14 +11,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public void getCatalog(String urlShop, String endPoint, String getByClass, String select, String href){
+    public void getCatalog(String urlShop, String endPoint, String getByClass, String select, String href, boolean needLink){
         Map<String, String> catalogMap = new HashMap<>();
 
         try {
+            Connection.Response response = Jsoup.connect(urlShop).method(Connection.Method.GET).execute();
+            System.out.println(response.body());
+
             Document page = Jsoup.connect(urlShop + endPoint)
 //                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-//                    .cookies(response.cookies())
+                    .cookies(response.cookies())
                     .get();
+
             Elements elements = page.getElementsByClass(getByClass);
 // Создание карты для хранения данных
 
@@ -28,10 +32,12 @@ public class Parser {
                 String link = element.attr(href);
                 catalogMap.put(text, link);
             }
-
+            if (needLink == false) {
+                urlShop = "";
+            }
             // Вывод карты
             for (Map.Entry<String, String> entry : catalogMap.entrySet()) {
-                System.out.println(entry.getKey() + " | " + urlShop + entry.getValue());
+//                System.out.println(entry.getKey() + " | " + urlShop + entry.getValue());
             }
 
         } catch (IOException e) {
--- src/main/java/ua/com/mobifix/parser/Run.java
+++ src/main/java/ua/com/mobifix/parser/Run.java
@@ -7,25 +7,37 @@ public class Run {
 //        String getByClass = "page_full-catalog_facet_title_link";
 //        String select = ".page_full-catalog_facet_title_text";
 //        String href = "href";
+//        boolean addLink = true;
 //
 //        String url = "https://www.aks.ua";
 //        String endPoint = "/uk/sitemap.html";
 //        String getByClass = "sitemap_title";
 //        String select = "a";
 //        String href = "href";
+//        boolean addLink = true;
 //
 //        String url = "https://hotline.ua";
 //        String endPoint = "/";
 //        String getByClass = "categories-section__link";
 //        String select = ".text";
 //        String href = "href";
+//        boolean addLink = true;
 
-        String url = "https://hotline.ua";
-        String endPoint = "/";
-        String getByClass = "categories-section__link";
-        String select = ".text";
+//        String url = "https://www.service-market.com.ua";
+//        String endPoint = "/";
+//        String getByClass = "main-nav__link";
+//        String select = "span.main-nav__title";
+//        String href = "href";
+//        boolean addLink = false;
+
+        String url = "https://www.moyo.ua";
+        String endPoint = "";
+        String getByClass = "menu_link";
+        String select = "a";
         String href = "href";
-        new Parser().getCatalog(url, endPoint, getByClass, select, href);
+        boolean needLink = true;
+
+        new Parser().getCatalog(url, endPoint, getByClass, select, href, needLink);
 
     }
 }
