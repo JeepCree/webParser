1bf56956e22e2df6fbd166426c22fdd32d7d9e3e
dima2 <dima.26.2013@gmail.com>
2023-12-26 20:53:34 +0200

    save 202312262053

---
+++ src/main/java/ua/com/mobifix/parser/CategoryParser.java
@@ -0,0 +1,145 @@
+package ua.com.mobifix.parser;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryParser {
+    public List<AllScanCategory> getCatalog(ScanCategorySettings settings, Long lastCategoryId){
+        lastCategoryId++;
+        List<AllScanCategory> categoryList = new ArrayList<>();
+
+        if (settings.getCookieName().equals("") || settings.getCookieValue().equals("")){
+            settings.setCookieName("noName");
+            settings.setCookieValue("noValue");
+        }
+
+        try {
+            Connection.Response response = Jsoup.connect(settings.getUrlShop()).method(Connection.Method.GET).execute();
+            Document page = Jsoup.connect(settings.getUrlShop())
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                    .cookie(settings.getCookieName(), settings.getCookieValue())
+                    .get();
+            Elements elements = page.select(settings.getSelectCategoryTag());
+            for (Element element : elements) {
+                String name = element.select(settings.getSelectCategoryNameTag()).text()
+                        .replace(settings.getReplaceCategoryName(), settings.getReplacementCategoryName());
+                String link = settings.getUrlPrefix() + element.select(settings.getSelectCategoryNameTag()).attr(settings.getSelectCategoryAttrHref())
+                        .replace(settings.getReplaceCategoryUrl(), settings.getReplacementCategoryUrl());
+                if (!link.isEmpty() || !name.isEmpty()) {
+                    AllScanCategory category = new AllScanCategory();
+                    category.setCategoryId(lastCategoryId);
+                    category.setCategoryName(name);
+                    category.setCategoryUrl(link);
+                    category.setParentCategoryId(0L);
+                    categoryList.add(category);
+                    lastCategoryId++;
+                    System.out.println(category.getCategoryId() + " " + category.getCategoryName() + " " + category.getCategoryUrl() + " " + category.getParentCategoryId());
+
+                    try {
+                        Document page2 = Jsoup.connect(link).get();
+                        Elements elements2 = page2.select(settings.getSelectCategoryTagLevel2());
+                        for (Element element2 : elements2) {
+                            String name2 = element2.select(settings.getSelectCategoryNameTagLevel2()).text()
+                                    .replace(settings.getReplaceCategoryNameLevel2(), settings.getReplacementCategoryNameLevel2());
+                            String link2 = settings.getUrlPrefix() + element2.select(settings.getSelectCategoryNameTagLevel2()).attr(settings.getSelectCategoryAttrHrefLevel2())
+                                    .replace(settings.getReplaceCategoryUrlLevel2(), settings.getReplacementCategoryUrlLevel2());
+                            if (!link.isEmpty() || !name.isEmpty()) {
+                                AllScanCategory category2 = new AllScanCategory();
+                                category2.setCategoryId(lastCategoryId);
+                                category2.setCategoryName(name2);
+                                category2.setCategoryUrl(link2);
+                                category2.setParentCategoryId(category.getCategoryId());
+                                categoryList.add(category2);
+                                lastCategoryId++;
+                                System.out.println(category2.getCategoryId() + " " + category2.getCategoryName() + " " + category2.getCategoryUrl() + " " + category2.getParentCategoryId());
+
+                                try {
+                                    Document page3 = Jsoup.connect(link2).get();
+                                    Elements elements3 = page3.select(settings.getSelectCategoryTagLevel3());
+                                    for (Element element3 : elements3) {
+                                        String name3 = element3.select(settings.getSelectCategoryNameTagLevel3()).text()
+                                                .replace(settings.getReplaceCategoryNameLevel3(), settings.getReplacementCategoryNameLevel3());
+                                        String link3 = settings.getUrlPrefix() + element3.select(settings.getSelectCategoryNameTagLevel3()).attr(settings.getSelectCategoryAttrHrefLevel3())
+                                                .replace(settings.getReplaceCategoryUrlLevel3(), settings.getReplacementCategoryUrlLevel3());
+                                        if (!link.isEmpty() || !name.isEmpty()) {
+                                            AllScanCategory category3 = new AllScanCategory();
+                                            category3.setCategoryId(lastCategoryId);
+                                            category3.setCategoryName(name3);
+                                            category3.setCategoryUrl(link3);
+                                            category3.setParentCategoryId(category2.getCategoryId());
+                                            categoryList.add(category3);
+                                            lastCategoryId++;
+                                            System.out.println(category3.getCategoryId() + " " + category3.getCategoryName() + " " + category3.getCategoryUrl() + " " + category3.getParentCategoryId());
+
+                                            try {
+                                                Document page4 = Jsoup.connect(link3).get();
+                                                Elements elements4 = page4.select(settings.getSelectCategoryTagLevel4());
+                                                for (Element element4 : elements4) {
+                                                    String name4 = element4.select(settings.getSelectCategoryNameTagLevel4()).text()
+                                                            .replace(settings.getReplaceCategoryNameLevel4(), settings.getReplacementCategoryNameLevel4());
+                                                    String link4 = settings.getUrlPrefix() + element4.select(settings.getSelectCategoryNameTagLevel4()).attr(settings.getSelectCategoryAttrHrefLevel4())
+                                                            .replace(settings.getReplaceCategoryUrlLevel4(), settings.getReplacementCategoryUrlLevel4());
+                                                    if (!link.isEmpty() || !name.isEmpty()) {
+                                                        AllScanCategory category4 = new AllScanCategory();
+                                                        category4.setCategoryId(lastCategoryId);
+                                                        category4.setCategoryName(name4);
+                                                        category4.setCategoryUrl(link4);
+                                                        category4.setParentCategoryId(category3.getCategoryId());
+                                                        categoryList.add(category4);
+                                                        lastCategoryId++;
+                                                        System.out.println(category4.getCategoryId() + " " + category4.getCategoryName() + " " + category4.getCategoryUrl() + " " + category4.getParentCategoryId());
+
+                                                        try {
+                                                            Document page5 = Jsoup.connect(link4).get();
+                                                            Elements elements5 = page5.select(settings.getSelectCategoryTagLevel5());
+                                                            for (Element element5 : elements5) {
+                                                                String name5 = element5.select(settings.getSelectCategoryNameTagLevel5()).text()
+                                                                        .replace(settings.getReplaceCategoryNameLevel5(), settings.getReplacementCategoryNameLevel5());
+                                                                String link5 = settings.getUrlPrefix() + element5.select(settings.getSelectCategoryNameTagLevel5()).attr(settings.getSelectCategoryAttrHrefLevel5())
+                                                                        .replace(settings.getReplaceCategoryUrlLevel5(), settings.getReplacementCategoryUrlLevel5());
+                                                                if (!link.isEmpty() || !name.isEmpty()) {
+                                                                    AllScanCategory category5 = new AllScanCategory();
+                                                                    category5.setCategoryId(lastCategoryId);
+                                                                    category5.setCategoryName(name5);
+                                                                    category5.setCategoryUrl(link5);
+                                                                    category5.setParentCategoryId(category4.getCategoryId());
+                                                                    categoryList.add(category5);
+                                                                    lastCategoryId++;
+                                                                    System.out.println(category5.getCategoryId() + " " + category5.getCategoryName() + " " + category5.getCategoryUrl() + " " + category5.getParentCategoryId());
+                                                                }
+                                                            }
+                                                        } catch (Exception e){
+                                                            e.printStackTrace();
+                                                        }
+                                                    }
+                                                }
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nКонец отработки модуля -> return categoryList\n");
+        return categoryList;
+    }
+}
+
--- src/main/java/ua/com/mobifix/parser/Run.java
+++ src/main/java/ua/com/mobifix/parser/Run.java
@@ -3,7 +3,7 @@ package ua.com.mobifix.parser;
 public class Run {
     public static void main(String[] args) {
         ScanCategorySettings settings = new ScanCategorySettings();
-        Parser parser = new Parser();
+        CategoryParser parser = new CategoryParser();
 //++
 //        String urlShop = "https://all-spares.ua";
 //        String endPoint = "/ru/full-catalog/";
@@ -68,13 +68,69 @@ public class Run {
 //        settings.setReplacementCategoryName("");
 
 //AKS
-        settings.setUrlShop("https://www.aks.ua");
-        settings.setSelectCategoryTag(".nav-list-block > ul > li > a");
-        settings.setSelectCategoryNameTag("a");
-        settings.setSelectCategoryAttrHref("href");
-        settings.setUrlPrefix("https://www.aks.ua");
+//        settings.setUrlShop("https://www.aks.ua");
+//        settings.setSelectCategoryTag(".nav-list-block > ul > li > a");
+//        settings.setSelectCategoryNameTag("a");
+//        settings.setSelectCategoryAttrHref("href");
+//        settings.setUrlPrefix("https://www.aks.ua");
+//        settings.setSelectCategoryTagLevel2("#mainContent > section.main > div > section.category > div > div > div.category-item > div.category-link");
+//        settings.setSelectCategoryNameTagLevel2("a");
+//        settings.setSelectCategoryAttrHrefLevel2("href");
+//        settings.setSelectCategoryTagLevel3("#mainContent > section.main > div > section.category > div > div > div.category-item > div.category-link");
+//        settings.setSelectCategoryNameTagLevel3("a");
+//        settings.setSelectCategoryAttrHrefLevel3("href");
+//        settings.setSelectCategoryTagLevel4("#mainContent > section.main > div > section.category > div > div > div.category-item > div.category-link");
+//        settings.setSelectCategoryNameTagLevel4("a");
+//        settings.setSelectCategoryAttrHrefLevel4("href");
+//        settings.setSelectCategoryTagLevel5("#mainContent > section.main > div > section.category > div > div > div.category-item > div.category-link");
+//        settings.setSelectCategoryNameTagLevel5("a");
+//        settings.setSelectCategoryAttrHrefLevel5("href");
+//all-spares
+//        settings.setUrlShop("https://all-spares.ua");
+//        settings.setSelectCategoryTag("body > div > div > header > div.widget_site-menu > div > div.menu-row > div > div > div.layout_header_quick-catalog_menu_nav > a");
+//        settings.setSelectCategoryNameTag("a");
+//        settings.setSelectCategoryAttrHref("href");
+//        settings.setUrlPrefix("https://all-spares.ua");
+//        settings.setSelectCategoryTagLevel2("body > div > div > div.sop-main-content > div > div > main > div.page_menu_category > div > div > article > div > a");
+//        settings.setSelectCategoryNameTagLevel2("a");
+//        settings.setSelectCategoryAttrHrefLevel2("href");
+//        settings.setSelectCategoryTagLevel3("body > div > div > div.sop-main-content > div > div > div > div.col-12.col-md-8.col-lg-9 > main > div.row.d-flex.product-cards-wrapper > div > div > div > a");
+//        settings.setSelectCategoryNameTagLevel3("a");
+//        settings.setSelectCategoryAttrHrefLevel3("href");
+//        settings.setSelectCategoryTagLevel4("body > div > div > div.sop-main-content > div > div > div > div.col-12.col-md-8.col-lg-9 > main > div.row.d-flex.product-cards-wrapper > div > div > div > a");
+//        settings.setSelectCategoryNameTagLevel4("a");
+//        settings.setSelectCategoryAttrHrefLevel4("href");
+//        settings.setSelectCategoryTagLevel5("body > div > div > div.sop-main-content > div > div > div > div.col-12.col-md-8.col-lg-9 > main > div.row.d-flex.product-cards-wrapper > div > div > div > a");
+//        settings.setSelectCategoryNameTagLevel5("a");
+//        settings.setSelectCategoryAttrHrefLevel5("href");
+//moyo
+//        String urlShop = "https://www.moyo.ua";
+//        String endPoint = "";
+//        String getByClass = "body > div.main-wrap > main > div > section > div.catalog-banner > div.catalog-container > ul > li";
+//        String select = "body > div.main-wrap > main > div > section > div.catalog-banner > div.catalog-container > ul > li > span";
+//        String href = "data-href";
+//        String urlPrefix = "https://www.moyo.ua";
+        settings.setUrlShop("https://www.moyo.ua");
+        settings.setSelectCategoryTag("body > div.main-wrap > main > div > section > div.catalog-banner > div.catalog-container > ul > li");
+        settings.setSelectCategoryNameTag("li > span");
+        settings.setSelectCategoryAttrHref("data-href");
+        settings.setUrlPrefix("https://www.moyo.ua");
+        settings.setSelectCategoryTagLevel2("#main-wrap > main > div.seo-order.container > div.portal-category-list > div");
+        settings.setSelectCategoryNameTagLevel2("div > a");
+        settings.setSelectCategoryAttrHrefLevel2("href");
+        settings.setSelectCategoryTagLevel3("#main-wrap > main > div.seo-order.container > div.portal-category-list > div > a.portal-category-list__title");
+        settings.setSelectCategoryNameTagLevel3("a");
+        settings.setSelectCategoryAttrHrefLevel3("href");
+        settings.setSelectCategoryTagLevel4("#main-wrap > main > div.seo-order.container > div.portal-category-list > div > a.portal-category-list__title");
+        settings.setSelectCategoryNameTagLevel4("a");
+        settings.setSelectCategoryAttrHrefLevel4("href");
+        settings.setSelectCategoryTagLevel5("#main-wrap > main > div.seo-order.container > div.portal-category-list > div > a.portal-category-list__title");
+        settings.setSelectCategoryNameTagLevel5("a");
+        settings.setSelectCategoryAttrHrefLevel5("href");
 
-        parser.getCatalog(settings, 1005L);
+        for (AllScanCategory el : parser.getCatalog(settings, 0L)) {
+            System.out.println(el.getCategoryId() + " " + el.getCategoryName() + " " + el.getCategoryUrl() + " " + el.getParentCategoryId());
+        }
 
     }
 }
