ed042b8b111e15e5b828ad6f12e78cf3866f3ea7
Dima <76869662+JeepCree@users.noreply.github.com>
2024-01-03 10:10:47 +0200

    save 202401031010

--- src/main/java/ua/com/mobifix/parser/test.java
+++ src/main/java/ua/com/mobifix/parser/test.java
@@ -14,32 +14,36 @@ import java.util.List;
 import java.util.Map;
 
 public class test {
-    public static void main(String[] args) throws IOException {
-        Document page = Jsoup.connect("https://all-spares.ua/ru/lcd-compatible-with-xiaomi-redmi-9a-black-with-touchscreen-original-prc-m2006c3lg/").get();
+    public static void main(String[] args) {
 
-        String el = page.select("div.gallery-main-wrap > div > picture > img").attr("src").replace("/480/", "/799/");
+            String page = "0";
+            String newPage = "1";
+            int i = 1;
+            boolean bool = true;
+            while (bool){
+                try {
+                        page = Jsoup.connect("https://all-spares.ua/uk/accessories/chargers/mains-chargers/?ipp=96&page=" + i)
+                                .get()
+                                .select("div.row.d-flex.product-cards-wrapper > div > div").text();
 
-        System.out.println(el);
-
-        String originalString = "Это пример строки, которую нужно изменить.";
-        String[] array = new String[2];
-
-        // Добавляем значения в массив
-        array[0] = "Значение1";
-        array[1] = "Значение2";
-
-
-        // Создаем список (List) массивов и добавляем в него наш массив
-        List<String[]> listOfArrays = new ArrayList<>();
-        listOfArrays.add(array);
-        System.out.println("Elements: " + listOfArrays.size());
-        for (String[] arr : listOfArrays) {
-            if (arr.length > 0) {
-                String firstElement = arr[0];
-                String secondElement = arr[1];
-                originalString = originalString.replace(firstElement, secondElement);
+                        newPage = Jsoup.connect("https://all-spares.ua/uk/accessories/chargers/mains-chargers/?ipp=96&page=" + (i + 1))
+                            .get()
+                            .select("div.row.d-flex.product-cards-wrapper > div > div").text();
+                        bool = !page.equals(newPage);
+                    System.out.println(!page.equals(newPage) + " " + i);
+                    i++;
+                } catch (IOException e) {
+                    System.out.println("ошибка Jsoup.connect...");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ie) {
+                        System.out.println("ошибка Thread.sleep(1000);");
+                    }
+                }
             }
-        }
-        System.out.println(originalString);
+            System.out.println("Конец сканирования!");
+
+
     }
 }
+
