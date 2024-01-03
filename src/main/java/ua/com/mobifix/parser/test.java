b50f95efd4bfc9c6d909218895461f2cd0bdd09c
Dima <76869662+JeepCree@users.noreply.github.com>
2024-01-03 15:35:38 +0200

    save 202401031535

--- src/main/java/ua/com/mobifix/parser/test.java
+++ src/main/java/ua/com/mobifix/parser/test.java
@@ -15,35 +15,43 @@ import java.util.Map;
 
 public class test {
     public static void main(String[] args) {
-
-            String page = "0";
-            String newPage = "1";
             int i = 1;
             boolean bool = true;
+            String url = "https://ukr-mobil.com/zapchasti_dlya_telefonov_10000086/displei_10000117/xiaomi_10000147?page=";
+            String select = "div.section.layout-box__catalog.catalog-line.test";
             while (bool){
                 try {
-                        page = Jsoup.connect("https://all-spares.ua/uk/accessories/chargers/mains-chargers/?ipp=96&page=" + i)
+                    String page = Jsoup.connect( url + i + "/")
                                 .get()
-                                .select("div.row.d-flex.product-cards-wrapper > div > div").text();
-
-                        newPage = Jsoup.connect("https://all-spares.ua/uk/accessories/chargers/mains-chargers/?ipp=96&page=" + (i + 1))
+                                .select(select).text();
+                    System.out.println("Сканируем страницу " + i);
+                    String newPage = Jsoup.connect(url + (i + 1) + "/")
                             .get()
-                            .select("div.row.d-flex.product-cards-wrapper > div > div").text();
-                        bool = !page.equals(newPage);
-                    System.out.println(!page.equals(newPage) + " " + i);
+                            .select(select).text();
+                    bool = !page.equals(newPage);
+//                    System.out.println(!page.equals(newPage) + " " + i);
                     i++;
                 } catch (IOException e) {
-                    System.out.println("ошибка Jsoup.connect...");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ie) {
-                        System.out.println("ошибка Thread.sleep(1000);");
+                    if (e instanceof org.jsoup.HttpStatusException) {
+                        org.jsoup.HttpStatusException httpStatusException = (org.jsoup.HttpStatusException) e;
+                        int statusCode = httpStatusException.getStatusCode();
+                        if (statusCode == 404) {
+                            System.out.println("Страница не найдена (ошибка 404).");
+                            bool = false; // Устанавливаем флаг в false, чтобы завершить цикл
+                        } else {
+                            System.out.println("Другая ошибка HTTP: " + statusCode);
+                        }
+                    } else {
+                        System.out.println("Ошибка сканирования. Посторное сканирование страницы...");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception ex) {
+                            System.out.println("ошибка Thread.sleep(1000);");
+                        }
                     }
                 }
             }
             System.out.println("Конец сканирования!");
-
-
     }
 }
 
